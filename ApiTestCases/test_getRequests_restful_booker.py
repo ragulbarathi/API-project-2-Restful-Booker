4b56885b59d872731f8d8a07be372748d5a094d1
ragulbarathi <ragulbarathi98@gmail.com>
2024-10-11 10:54:27 +0530

    updated tc's

--- ApiTestCases/test_getRequests_restful_booker.py
+++ ApiTestCases/test_getRequests_restful_booker.py
@@ -7,13 +7,13 @@ import requests
 @allure.title("TC1-Verify ping request")
 @allure.description("TC1-healthcheck of the app")
 @allure.tag("smoke")
-@allure.severity("Major")
+@allure.severity(allure.severity_level.BLOCKER)
 def test_get_healthcheck_booker():
     url = "https://restful-booker.herokuapp.com/ping"
     response_data = requests.get(url)
     print(response_data.text)
     print(response_data.headers)
-    assert response_data.status_code == 201
+    assert response_data.status_code == 201, f"expected status code is 201 but got {response_data.status_code}"
 
 
 @pytest.mark.smoke
@@ -21,7 +21,7 @@ def test_get_healthcheck_booker():
 @allure.description("TC2-Tests the booking details of the user with an valid booking id and status code should be 200, "
                     "this is a positive tc")
 @allure.tag("smoke")
-@allure.severity("Major")
+@allure.severity(allure.severity_level.CRITICAL)
 def test_get_booking_valid_booking_id():
     url = "https://restful-booker.herokuapp.com/booking/5"
     response_data = requests.get(url)
@@ -35,40 +35,53 @@ def test_get_booking_valid_booking_id():
 @allure.description("TC3-Tests the booking details of the user with an null booking id and status code should be 404, "
                     "this is a negative tc")
 @allure.tag("smoke")
-@allure.severity("Major")
+@allure.severity(allure.severity_level.CRITICAL)
 def test_get_booking_null_booking_id():
     url = "https://restful-booker.herokuapp.com/booking/null"
     response_data = requests.get(url)
     print(response_data.text)
     print(response_data.headers)
-    assert response_data.status_code == 404
+    assert response_data.status_code == 404, f"expected status code 404 but found {response_data.status_code}"
 
 
 @pytest.mark.smoke
-@allure.title("TC4-Verify booking details are not shown for negative booking id")
-@allure.description("TC4-Tests the booking details of the user with an negative number booking id and status code "
+@allure.title("TC4-Verify booking details are not shown for empty booking id")
+@allure.description("TC4-Tests the booking details of the user with an empty booking id and status code should be 404, "
+                    "this is a negative tc")
+@allure.tag("smoke")
+@allure.severity(allure.severity_level.CRITICAL)
+def test_get_booking_null_booking_id():
+    url = "https://restful-booker.herokuapp.com/booking/"
+    response_data = requests.get(url)
+    print(response_data.text)
+    print(response_data.headers)
+    assert response_data.status_code == 404, f"expected status code 404 but found {response_data.status_code}"
+
+
+@pytest.mark.smoke
+@allure.title("TC5-Verify booking details are not shown for negative booking id")
+@allure.description("TC5-Tests the booking details of the user with an negative number booking id and status code "
                     "should be 404,"
                     "this is a negative tc")
 @allure.tag("smoke")
-@allure.severity("Major")
+@allure.severity(allure.severity_level.CRITICAL)
 def test_get_booking_negative_booking_id():
     url = "https://restful-booker.herokuapp.com/booking/-5"
     response_data = requests.get(url)
     print(response_data.text)
     print(response_data.headers)
-    assert response_data.status_code == 404
+    assert response_data.status_code == 404, f"expected status code 404 but found {response_data.status_code}"
 
 
 @pytest.mark.smoke
-@allure.title("TC5-Verify booking details are not shown for invalid booking id")
-@allure.description("TC5-Tests the booking details of the user with an invalid booking id and status code "
-                    "should be 404,"
-                    "this is a negative tc")
+@allure.title("TC6-Verify booking details are not shown for invalid booking id with special characters.")
+@allure.description("TC6-Tests the booking details of the user with an invalid booking id with special characters "
+                    "and status code should be 404,this is a negative tc")
 @allure.tag("smoke")
-@allure.severity("Major")
+@allure.severity(allure.severity_level.CRITICAL)
 def test_get_booking_special_char_booking_id():
     url = "https://restful-booker.herokuapp.com/booking/!@"
     response_data = requests.get(url)
     print(response_data.text)
     print(response_data.headers)
-    assert response_data.status_code == 404
+    assert response_data.status_code == 404, f"expected status code 404 but found {response_data.status_code}"
