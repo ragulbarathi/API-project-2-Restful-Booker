ca32fa2953ec395f588c726802d8f4ca53e6ea33
ragulbarathi <ragulbarathi98@gmail.com>
2024-10-11 14:56:21 +0530

    updated conftest

--- ApiTestCases/test_crud.py
+++ ApiTestCases/test_crud.py
@@ -8,21 +8,5 @@ import allure
 @allure.description("Verify create booking")
 @allure.tag("smoke")
 @allure.severity(allure.severity_level.BLOCKER)
-def test_create_booking():
-    URL = "https://restful-booker.herokuapp.com/booking"
-    HEADERS = {"Content-Type": "application/json"}
-    DATA = {
-        "firstname": "Ragul",
-        "lastname": "Ravibarathi",
-        "totalprice": 111,
-        "depositpaid": True,
-        "bookingdates": {
-            "checkin": "2024-01-01",
-            "checkout": "2024-02-01"
-        },
-        "additionalneeds": "Breakfast"
-    }
-    response = requests.post(url=URL, headers=HEADERS, json=DATA)
-    booking_id = response.json()["bookingid"]
-    print(booking_id)
-    return booking_id
+def test_create_booking(create_booking_id):
+    pass
