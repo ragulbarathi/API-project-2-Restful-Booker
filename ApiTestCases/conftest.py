58f7d8aec2287c094411f2f50323482d7ca65c74
ragulbarathi <ragulbarathi98@gmail.com>
2024-10-11 14:29:10 +0530

    updated conftest

--- ApiTestCases/conftest.py
+++ ApiTestCases/conftest.py
@@ -1,23 +1,33 @@
-import allure
 import pytest
 import requests
-
-
-@pytest.fixture
-def baseurl():
-    return "https://restful-booker.herokuapp.com/booking"
-
-
-@pytest.fixture
-def basepath():
-    return create_booking()
+from dotenv import load_dotenv
+import os
 
 
 @pytest.fixture()
-def create_booking():
-    URL = baseurl()
-    HEADERS = {'Content-Type: application/json'}
-    BODY = {
+def create_token():
+    load_dotenv()
+    username = os.getenv("USERNAME")
+    password = os.getenv("PASSWORD")
+
+    print("Creating Token....")
+    url = "https://restful-booker.herokuapp.com/auth"
+    headers = {"Content-Type": "application/json"}
+    json_payload = {
+        "username": username,
+        "password": password
+    }
+    response = requests.post(url=url, headers=headers, json=json_payload)
+    token = response.json()["token"]
+    print(token)
+    return token
+
+
+@pytest.fixture()
+def create_booking_id():
+    URL = "https://restful-booker.herokuapp.com/booking"
+    HEADERS = {"Content-Type": "application/json"}
+    DATA = {
         "firstname": "Ragul",
         "lastname": "Ravibarathi",
         "totalprice": 111,
@@ -28,7 +38,12 @@ def create_booking():
         },
         "additionalneeds": "Breakfast"
     }
-    response = requests.post(url=URL, headers=HEADERS, data=BODY)
-    assert response.status_code == 201, f" Expected 201 but found {response.status_code}"
-    booking_id=response.json()["bookingid"]
+    response = requests.post(url=URL, headers=HEADERS, data=DATA)
+    booking_id = response.json()["bookingid"]
+    print(booking_id)
     return booking_id
+
+
+def test_consume(create_token, create_booking_id):
+    print("Token->", create_token)
+    print("Booking id->", create_booking_id)
