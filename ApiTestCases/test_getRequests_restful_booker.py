c707e7306404ddf5fd6befae2d716f357282c0a6
ragulbarathi <ragulbarathi98@gmail.com>
2024-10-11 10:58:02 +0530

    updated tc's

--- ApiTestCases/test_getRequests_restful_booker.py
+++ ApiTestCases/test_getRequests_restful_booker.py
@@ -27,7 +27,7 @@ def test_get_booking_valid_booking_id():
     response_data = requests.get(url)
     print(response_data.json())
     print(response_data.headers)
-    assert response_data.status_code == 200
+    assert response_data.status_code == 200, f"expected status code is 200 but got {response_data.status_code}"
 
 
 @pytest.mark.smoke
@@ -45,22 +45,8 @@ def test_get_booking_null_booking_id():
 
 
 @pytest.mark.smoke
-@allure.title("TC4-Verify booking details are not shown for empty booking id")
-@allure.description("TC4-Tests the booking details of the user with an empty booking id and status code should be 404, "
-                    "this is a negative tc")
-@allure.tag("smoke")
-@allure.severity(allure.severity_level.CRITICAL)
-def test_get_booking_null_booking_id():
-    url = "https://restful-booker.herokuapp.com/booking/"
-    response_data = requests.get(url)
-    print(response_data.text)
-    print(response_data.headers)
-    assert response_data.status_code == 404, f"expected status code 404 but found {response_data.status_code}"
-
-
-@pytest.mark.smoke
-@allure.title("TC5-Verify booking details are not shown for negative booking id")
-@allure.description("TC5-Tests the booking details of the user with an negative number booking id and status code "
+@allure.title("TC4-Verify booking details are not shown for negative booking id")
+@allure.description("TC4-Tests the booking details of the user with an negative number booking id and status code "
                     "should be 404,"
                     "this is a negative tc")
 @allure.tag("smoke")
@@ -74,8 +60,8 @@ def test_get_booking_negative_booking_id():
 
 
 @pytest.mark.smoke
-@allure.title("TC6-Verify booking details are not shown for invalid booking id with special characters.")
-@allure.description("TC6-Tests the booking details of the user with an invalid booking id with special characters "
+@allure.title("TC5-Verify booking details are not shown for invalid booking id with special characters.")
+@allure.description("TC5-Tests the booking details of the user with an invalid booking id with special characters "
                     "and status code should be 404,this is a negative tc")
 @allure.tag("smoke")
 @allure.severity(allure.severity_level.CRITICAL)
